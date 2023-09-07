dfc724ad849fbc52ca08af2f1e1b51e51dd11d61
FranCampertoni <francampertoni@gmail.com>
2023-09-07 18:44:38 -0300

    actualizado hasta el component

---
+++ src/app/services/test.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient, HttpResponse, HttpParams} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+  private url = 'https://228e-181-231-122-56.ngrok-free.app/student'
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<any>{
+    return this.http.get(this.url + '/getAll')
+  }
+}
+
